03612d100555a55f2a470b0973efbf875efedcc0
madagadiop <madagadiop34@gmail.com>
2023-02-10 01:15:58 +0000

    finalisation de la page inscription

--- lib/screens/page_inscription.dart
+++ lib/screens/page_inscription.dart
@@ -1,14 +1,164 @@
 import 'package:flutter/material.dart';
 
+class PageInscription extends StatefulWidget {
+  @override
+State<StatefulWidget> createState() => _PageInscription();
 
-class PageInscription extends StatelessWidget {
-  const PageInscription({super.key});
+}
+
+class _PageInscription extends State<PageInscription> {
+  // Cr�ez un contr�leur pour le champ de formulaire pour obtenir des valeurs de retour
+  final _formKey = GlobalKey<FormState>();
+
+  // Variables pour stocker les entr�es de l'utilisateur
+  String _prenom ="";
+  String _nom ="";
+  String _username = "";
+  String _email = "";
+  String _password ="";
+  String _confirmpassword ="";
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PAGE INSCRIPTION')
+      body: Column(
+        children: [
+         const Card(
+           child: Padding(
+           padding: EdgeInsets.all(16.0),
+           ),
+         ),
+          
+          const Image(
+            image: AssetImage('lib/assets/images/dolibarr_mobile.JPG'),
+            ),
+          Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Champ pour le prenom
+                  TextFormField(
+                   decoration: InputDecoration(labelText: 'Prenom'),
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez entrer votre prenom';
+                      }
+                    return null;
+                  },
+                  
+                ),
+                // Champ pour nom
+                
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nom'),
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez entrer votre nom';
+                    }
+                    return null;
+                  },
+                  
+                ),
+                // Champ pour le nom d'utilisateur
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nom d\'utilisateur'),
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez entrer un nom d\'utilisateur';
+                    }
+                    return null;
+                  },
+                  
+                ),
+                // Champ pour l'adresse e-mail
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Adresse email'),
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez entrer une adresse e-mail';
+                    }
+                    return null;
+                  },
+                  
+                ),
+                // Champ pour le mot de passe
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Mot de passe'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez entrer un mot de passe';
+                    }
+                    return null;
+                  },
+                  
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Confirmer mot de passe'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value !=null && value.isEmpty) {
+                      return 'Veuillez confirmer votre mot de passe';
+                    }
+                    return null;
+                  },
+                  
+                ),
+                // Bouton d'inscription
+                Container(
+                      height: 30,
+                      margin: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(5),
+                                 ),
+                    child: TextButton(
+                    style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                                 (Set<MaterialState> states) {
+                                 if (states.contains(MaterialState.hovered))
+                              return Colors.blue.withOpacity(0.04);
+                              if (states.contains(MaterialState.focused) ||
+                                states.contains(MaterialState.pressed))
+                                return Colors.blue.withOpacity(0.12);
+                       return null;
+                },
+               ),
+              ),
+             onPressed: () {
+               Navigator.push(
+             context,
+             MaterialPageRoute(builder: (context) => PageSucces()),
+             );
+             },
+            child: Text('ENREGISTRER'),
+           ),
+          ),
+     
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+    
+  }
+}
+class PageSucces extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Inscription réussie !",style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.lightBlue,
+            )
+            ),
+        
       ),
     );
   }
