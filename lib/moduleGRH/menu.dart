c59a6f8133bb8bdb3de275b708175697136f0fbb
soxnantamousellmi <bbantitidiop@gmail.com>
2023-02-16 23:37:04 +0100

    page connexion terminee

---
+++ lib/moduleGRH/menu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+
+class PageMenu extends StatefulWidget {
+  const PageMenu({super.key});
+
+  @override
+  State<PageMenu> createState() => _PageMenu();
+}
+
+class _PageMenu extends State<PageMenu> {
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar());}}
